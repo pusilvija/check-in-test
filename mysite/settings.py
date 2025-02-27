37537c038a0004920309a4af2a607f5349f91df7
pusilvija <silvijapupsaite@gmail.com>
2025-02-27 12:38:54 +0200

    css fixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -158,8 +158,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # For production, you would use this:
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "polls/static"),
+                    os.path.join(BASE_DIR, "static")]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

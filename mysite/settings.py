aee2cc965b476c119339b5e02253a2a6a6f3c8b1
pusilvija <silvijapupsaite@gmail.com>
2025-02-28 17:33:44 +0200

    debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEBUG", False)
 # DEBUG = False
-DEBUG = os.getenv("DJANGO_DEBUG", "True")
+DEBUG = False #os.getenv("DJANGO_DEBUG", "True")
 
 print("DEBUG: ", DEBUG)
 
